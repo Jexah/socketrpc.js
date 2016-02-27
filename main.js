6d31a1cb0aa767597b7b310f1561bfe010f30ac0
Jeremy Short <ijexah@gmail.com>
2016-02-28 09:00:38 +1100

    Moved to socketio base.

--- main.js
+++ main.js
@@ -5,15 +5,15 @@ const websocket = require('nodejs-websocket');
 const wsHooks = {};
 
 // Initialize the websocket server on port, return the function to register procedures.
-module.exports = (port, debug) => {
-  websocket.createServer().listen(port).on('connection', (ws) => {
-    ws.on('text', (data) => {
-      const obj = JSON.parse(data);
+module.exports = (io, debug) => {
+  io.on('connection', socket => {
+    if(debug) console.log(`Socket connected.`);
+    socket.on('rpc', obj => {
       if(Object.keys(wsHooks).indexOf(obj.procedureName) != -1){
         new Promise((resolve, reject) => { 
           wsHooks[obj.procedureName](...obj.args, resolve, reject);
         }).then(val => {
-          ws.sendText(JSON.stringify({uid: obj.uid, val: val}));
+          socket.emit('rpc', {uid: obj.uid, val: val});
         });
       }else{
         if(debug) console.log(`Procedure ${obj.procedureName} not found.`);
